57a449bd734470cbfa0c2faaea4d1e9a30309f94
sasensi <asensi.samuel@gmail.com>
2018-10-01 10:03:39 +0200

    update failing tests according to app behaviour change

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -55,8 +55,31 @@ describe('AppComponent', () =>
         expect(titles).toEqual([ '3', 'A' ]);
     }));
 
-    it('should scroll to about preview when 3 or A is clicked', () =>
+    it('should display random image on enter', async(() =>
     {
+        expect(fixture.debugElement.query(By.css('app-random-image'))).not.toBeNull();
+    }));
+
+    it('should display main page and hide random image when 3 or A is clicked', () =>
+    {
+        // click on title
+        fixture.debugElement.query(By.css('h1')).triggerEventHandler('click', null);
+        fixture.detectChanges();
+
+        expect(fixture.debugElement.query(By.css('app-main-page'))).not.toBeNull();
+        expect(fixture.debugElement.query(By.css('app-random-image'))).toBeNull();
+    });
+
+    it('should scroll to about preview when 3 or A is clicked when main page is displayed', () =>
+    {
+        // click on title to display main page
+        fixture.debugElement.query(By.css('h1')).triggerEventHandler('click', null);
+        fixture.detectChanges();
+
+        // reclick on title
+        fixture.debugElement.query(By.css('h1')).triggerEventHandler('click', null);
+        fixture.detectChanges();
+
         const scrollService = TestBed.get(ScrollService) as ScrollService;
         const titleNodes    = fixture.debugElement.queryAll(By.css('h1'));
         spyOn(scrollService, 'scrollToAbout');
